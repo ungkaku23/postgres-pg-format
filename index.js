b2cbdc1062cea61c141731e8e8fcc3b620291d5c
james23216 <jhmun23216@gmail.com>
2022-05-18 00:56:52 -0700

    update with performance

--- index.js
+++ index.js
@@ -7,15 +7,8 @@ const updateData = require('./data');
 const format = require('pg-format');
 
 async function runQuery() {
-
-  // Create Temp Table
-  await db.query(format(`CREATE TEMP TABLE temp_user (id integer, age integer)`));
-  await db.query(format('INSERT INTO temp_user (id, age) VALUES %L', updateData.data));
-
-  await db.query(`UPDATE users AS u 
-                  SET age = tu.age
-                  FROM temp_user AS tu
-                  WHERE u.id = tu.id `);
+  let data = await db.query(format('UPDATE users AS u SET age = t2.age FROM (SELECT id::int, age::int FROM (VALUES %L) AS t(id, age)) AS t2 WHERE t2.id = u.id', updateData.data));
+  console.log(helper.emptyOrRows(data));
 }
 
 runQuery();
